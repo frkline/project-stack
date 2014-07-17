11340b15ca9a41cf7c7accb30b053b7a6387dbdb
Frank-Robert Kline <frank-robert.kline@hightail.com>
2014-07-17 11:48:52 -0700

    moving to updated nginx

--- cookbook/spec/nginx_spec.rb
+++ cookbook/spec/nginx_spec.rb
@@ -1,11 +1,2 @@
 require 'chefspec'
 require_relative 'spec_helper'
-
-describe 'project-stack::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
-
-  it 'includes the nginx::source recipe' do
-    expect(chef_run).to include_recipe('nginx::source')
-  end
-
-end
