1f7368a75f04395b39ec7ca75bdf6dc92b7db6af
Frank-Robert Kline <frank-robert.kline@hightail.com>
2014-07-18 02:45:21 -0700

    starting ng app and project buildout. fixed vagrant setup

---
+++ source/stack-account/src/main/java/com/frkline/project/stack/account/App.java
@@ -0,0 +1,13 @@
+package com.frkline.project.stack.account;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ source/stack-app/app/scripts/services/myService.js
@@ -0,0 +1,3 @@
+/**
+ * Created by frank on 7/18/14.
+ */
